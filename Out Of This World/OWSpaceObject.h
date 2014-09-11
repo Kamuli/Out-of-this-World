bbcdcc2ec4630d8dcb27b30644d37178c785508a
Kamuli Software <info@kamuli.com>
2014-09-11 19:08:32 +0300

    lista valmiina

---
+++ Out Of This World/OWSpaceObject.h
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.h
+//  Out Of This World
+//
+//  Created by Juha Näppi on 11.9.2014.
+//  Copyright (c) 2014 Kamuli Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OWSpaceObject : NSObject
+
+@end
---
+++ Out Of This World/OWSpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.m
+//  Out Of This World
+//
+//  Created by Juha Näppi on 11.9.2014.
+//  Copyright (c) 2014 Kamuli Software. All rights reserved.
+//
+
+#import "OWSpaceObject.h"
+
+@implementation OWSpaceObject
+
+@end
